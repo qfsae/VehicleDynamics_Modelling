0d9703a747fb875df3ca7295e64c57a6344336a5
Maurice Nayman <maurice.nayman@queensu.ca>
2022-02-21 18:56:32 -0500

    Corrections to calculateLateralAccelInterp.m to improve the starting point for the lateral acceleration search

--- LapsimAndYMD/Functions/calculateLateralAccelInterp.m
+++ LapsimAndYMD/Functions/calculateLateralAccelInterp.m
@@ -3,7 +3,7 @@ function [ay, mz, mu] = calculateLateralAccelInterp(vx, steerAngle, bodySlip)
     aTMP = linspace(-aMax, aMax, 400);
     aOut = calculateAccelerationAndMoment(aTMP, vx, steerAngle, bodySlip) - aTMP;
     
-    signChanges = find(sign(aOut(1:end-1)) ~= sign(aOut(2:end)), 1);
+    signChanges = find(sign(aOut(1:end-1)) ~= sign(aOut(2:end)));
     
     if isempty(signChanges) == 1
         %This will give an offset to ensure the zero is found correctly in
@@ -12,10 +12,10 @@ function [ay, mz, mu] = calculateLateralAccelInterp(vx, steerAngle, bodySlip)
         ay = aOut(idx) + aTMP(idx);
         [~, mz, mu] = calculateAccelerationAndMoment(ay, vx, steerAngle, bodySlip);
     else
-        [~, idx] = min(abs(aOut));
+        [~, idx] = max(abs(aTMP(signChanges)));
     
         f = @(x) pchip(aTMP, aOut, x);
-        [xZero, ay] = fzero(f, aTMP(idx));
+        [xZero, ay] = fzero(f, aTMP(signChanges(idx)));
     
         ay = xZero + ay;
         [~, mz, mu] = calculateAccelerationAndMoment(ay, vx, steerAngle, bodySlip);
